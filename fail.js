d0e9c9fde219cf5b67f84eb3da9d94a9c557d3fc
Genvictus <johannstarkmk42@gmail.com>
2022-11-26 00:00:33 +0700

    laporan & testcases

---
+++ fail.js
@@ -0,0 +1,17 @@
+
+   // This is a sample comment
+   if (x == 0) {
+     var a = 0;
+   } else if (x + 4 == 1) {
+     if (true) {
+       let b = 3;
+     } else {
+       break;
+     }
+   } else {
+     x == 2
+     "Momen";
+     
+   }
+ 
+ 
