a1342f6749afeab7e830b143b185b1e1555a5af4
Haziq <haziqabiyyu@gmail.com>
2022-11-25 14:19:27 +0700

    Masukkin Expr_opr ke NodeJS Grammar

--- io.py
+++ io.py
@@ -1,17 +1,28 @@
 # belum selesai
 
-ignore = ["\n", "\t", " "]
-reserved = []
+#ignore = ["\t", " "]
+reserved = ["break", "const", "case", "case", "catch", "class", "continue", "default", "delete", "else", "false", "finally", "for", "function", "if", "let", "null", "return", "switch", "throw", "try", "true", "var", "while"]
 
 def fileToLines(fileName) :
     buffer = []
-    ctr = 0
+    # currentWord = ""
+    tempCharList = []
     try :
         f = open(fileName, 'r')
         line = f.readline()
         while (line) :
-            buffer.append(line)
-            ctr += 1
+            for word in (line.split()) :
+                if word in reserved :
+                    buffer.append(word)
+                else :
+                    if len(word) == 1 :
+                        buffer.append(word)
+                    else :
+                        for char in word :
+                            tempCharList.append(char)
+                        buffer.append(tempCharList)
+                        tempCharList = []
+            buffer.append('\n')
             line = f.readline()
         return buffer
     except FileNotFoundError:
@@ -40,9 +51,9 @@ def convertLineToList(line) :
 
 
 if __name__ == "__main__" :
-    #text = fileToLines("inputAcc.js")
-    w = "function foo(x) { \n {x = 1} \n}"
-    print(w)
-    print(convertLineToList(w))
+    text = fileToLines("inputAcc.js")
+    # w = "function foo(x) { \n {x = 1} \n}"
+    # print(w)
+    # print(convertLineToList(w))
 
-    #["function foo(x)", "{", "{", "x = 1", "}", "}"]
+    print(text)
