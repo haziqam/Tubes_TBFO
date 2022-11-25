020e61a052cd8907d605a5334465ebb1ae3d91a7
Genvictus <johannstarkmk42@gmail.com>
2022-11-25 21:31:28 +0700

    update: rule now includes statements

--- rules.py
+++ rules.py
@@ -134,6 +134,9 @@ R = {
     "A8": [["A9", "A6"]],
     "A9": [["+"], ["-"], ["/"], ["*"], ["%"], ["&"], ["|"], ["^"], ["B8", "B8"], ["B9", "B9"], ["B10", "B10"], ["B11", "B11"], ["B12", "B12"]],
 
+    "CURL1": [["{"]],
+    "CURL2": [["}"]],
+
     # basic function
     "FU": [["FU1", "FU2"]],
     "FU1": [["function"]],
@@ -224,4 +227,154 @@ R = {
     "CO7": [[","]],
     "CO8": [["CO9", "STMTSW"]],
     "CO9": [[")"]],
+
+    # Function Call
+    "FCALL": [["FU1","FU2"],["AF1","AF2"],["AF3","AF10"],["V","AF10"]],
+
+    # Extra symbols
+    "CMA": [[","]],
+    "CLN": [[":"]],
+    "SCLN": [[";"]],
+    "CURL1": [["{"]],
+    "CURL2": [["}"]],
+
+    # STATEMENT KEYWORDS
+    "RTN": [["return"]],
+    "BRK": [["break"]],
+    "CNTN": [["continue"]],
+    "THR": [["throw"]],
+    "IF": [["if"]],
+    "ELSE": [["else"]],
+    "SWTC": [["switch"]],
+    "CASE": [["case"]],
+    "DFLT": [["default"]],
+    "TRY": [["try"]],
+    "CATCH": [["catch"]],
+    "FNLY": [["finally"]],
+
+    "VAR": [["var"]],
+    "LET": [["let"]],
+    "CONST": [["const"]],
+
+    "WHL": [["while"]],
+    "FOR": [["for"]],
+
+    # Statement call    
+    "STMT": [["CURL1","BLK2"],["THR","E"],["IF","IF2"],["SWTC","SWTC2"],["TRY","TRY2"],["VAR","V"],["VAR","A"],["LET","V"],["LET","A"],["CONST","V"],["CONST","A"],["FOR","FOR2"],["WHL","WHL2"]],
+
+    "STMTSW": [["CURL1","BLKSW2"],["THR","E"],["IF","IFSW2"],["SWTC","SWTC2"],["TRY","TRYSW2"],["VAR","V"],["VAR","A"],["LET","V"],["LET","A"],["CONST","V"],["CONST","A"],["FOR","FOR2"],["WHL","WHL2"],["break"]],
+
+    "STMTL": [["CURL1","BLKL2"],["THR","E"],["IF","IFL2"],["SWTCL","SWTCL2"],["TRY","TRYL2"],["VAR","V"],["VAR","A"],["LET","V"],["LET","A"],["CONST","V"],["CONST","A"],["FOR","FOR2"],["WHL","WHL2"],["break"],["continue"]],
+
+    # Blocks
+    "BLK": [["CURL1","BLK2"],["CURL1","CURL2"]],
+    "BLK2": [["START","CURL2"]],
+
+    "BLKSW": [["CURL1","BLKSW2"]],
+    "BLKSW2": [["STARTSW","CURL2"]],
+
+    "BLKL": [["CURL1","BLKL2"]],
+    "BLKL2": [["STARTL","CURL2"]],
+
+    # Throw
+    "THR": [["THR","E"]],
+
+    # If
+    "IF1": [["IF","IF2"]],
+    "IF2": [["E2","IF3"]],
+    "IF3": [["E","IF4"]],
+    "IF4": [["E4","IF5"],["E4","STMT"],["E4","E"],["E4","FCALL"]],
+    "IF5": [["STMT","IF6"],["E","IF6"],["FCALL","IF6"]],
+    "IF6": [["ELSE","STMT"],["ELSE","E"],["ELSE","FCALL"]],
+
+    "IFSW1": [["IF","IFSW2"]],
+    "IFSW2": [["E2","IFSW3"]],
+    "IFSW3": [["E","IFSW4"]],
+    "IFSW4": [["E4","IFSW5"],["E4","STMTSW"],["E4","E"],["E4","FCALL"]],
+    "IFSW5": [["STMTSW","IFSW6"],["E","IFSW6"],["FCALL","IFSW6"]],
+    "IFSW6": [["ELSE","STMTSW"],["ELSE","E"],["ELSE","FCALL"]],
+
+    "IFL1": [["IF","IFL2"]],
+    "IFL2": [["E2","IFL3"]],
+    "IFL3": [["E","IFL4"]],
+    "IFL4": [["E4","IFL5"],["E4","STMTL"],["E4","E"],["E4","FCALL"]],
+    "IFL5": [["STMTL","IFL6"],["E","IFL6"],["FCALL","IFL6"]],
+    "IFL6": [["ELSE","STMTL"],["ELSE","E"],["ELSE","FCALL"]],
+
+    # Switch
+    "SWTC1": [["SWTC","SWTC2"]],
+    "SWTC2": [["E2","SWTC3"]],
+    "SWTC3": [["E","SWTC4"]],
+    "SWTC4": [["E4","SWTC5"],["CMA","SWTC3"]],
+    "SWTC5": [["CURL1","SWTC6"]],
+    "SWTC6": [["CASE","SWTC7"],["DEFAULT","SWTC11"]],
+    "SWTC7": [["E","SWTC8"]],
+    "SWTC8": [["CLN","SWTC9"]],
+    "SWTC9": [["STARTSW","SWTC6"],["STARTSW","CURL2"]],
+    "SWTC10": [["CASE","SWTC11"]],
+    "SWTC11": [["E","SWTC12"]],
+    "SWTC12": [["CLN","SWTC13"]],
+    "SWTC13": [["STARTSW","SWTC10"],["STARTSW","CURL2"]],
+    
+    "SWTCL1": [["SWTCL","SWTCL2"]],
+    "SWTCL2": [["E2","SWTCL3"]],
+    "SWTCL3": [["E","SWTCL4"]],
+    "SWTCL4": [["E4","SWTCL5"],["CMA","SWTCL3"]],
+    "SWTCL5": [["CURL1","SWTCL6"]],
+    "SWTCL6": [["CASE","SWTCL7"],["DEFAULT","SWTCL11"]],
+    "SWTCL7": [["E","SWTCL8"]],
+    "SWTCL8": [["CLN","SWTCL9"]],
+    "SWTCL9": [["STARTL","SWTCL6"],["STARTL","CURL2"]],
+    "SWTCL10": [["CASE","SWTCL11"]],
+    "SWTCL11": [["E","SWTCL12"]],
+    "SWTCL12": [["CLN","SWTCL13"]],
+    "SWTCL13": [["STARTL","SWTCL10"],["STARTL","CURL2"]],
+
+    # Try
+    "TRY1": [["TRY","TRY2"]],
+    "TRY2": [["BLK","TRY3"]],
+    "TRY3": [["CATCH","BLK"],["CATCH","TRY4"],["CATCH","TRY8"],["FNLY","BLK"]],
+    "TRY4": [["E2","TRY5"]],
+    "TRY5": [["V","TRY6"]],
+    "TRY7": [["E4","BLK"],["E4","TRY8"]],
+    "TRY8": [["BLK","TRY9"]],
+    "TRY9": [["FNLY","BLK"]],
+
+    "TRYSW1": [["TRY","TRYSW2"]],
+    "TRYSW2": [["BLKSW","TRYSW3"]],
+    "TRYSW3": [["CATCH","BLKSW"],["CATCH","TRYSW4"],["CATCH","TRYSW8"],["FNLY","BLKSW"]],
+    "TRYSW4": [["E2","TRYSW5"]],
+    "TRYSW5": [["V","TRYSW6"]],
+    "TRYSW7": [["E4","BLKSW"],["E4","TRYSW8"]],
+    "TRYSW8": [["BLKSW","TRYSW9"]],
+    "TRYSW9": [["FNLY","BLKSW"]],
+
+    "TRYL1": [["TRY","TRYL2"]],
+    "TRYL2": [["BLKL","TRYL3"]],
+    "TRYL3": [["CATCH","BLKL"],["CATCH","TRYL4"],["CATCH","TRYL8"],["FNLY","BLKL"]],
+    "TRYL4": [["E2","TRYL5"]],
+    "TRYL5": [["V","TRYL6"]],
+    "TRYL7": [["E4","BLKL"],["E4","TRYL8"]],
+    "TRYL8": [["BLKL","TRYL9"]],
+    "TRYL9": [["FNLY","BLKL"]],
+
+    # Declarations
+    "DCLR": [["VAR","V"],["VAR","A"],["LET","V"],["LET","A"],["CONST","V"],["CONST","A"]],
+
+    # For loop
+    "FOR1": [["FOR","FOR2"]],
+    "FOR2": [["E2","FOR3"],["E2","FOR4"]],
+    "FOR3": [["E","FOR4"]],
+    "FOR4": [["SCLN","FOR5"],["SCLN","FOR6"]],
+    "FOR5": [["E","FOR6"],["DCLR","FOR6"]],
+    "FOR6": [["SCLN","FOR7"],["SCLN","FOR8"]],
+    "FOR7": [["E","FOR8"]],
+    "FOR8": [["E4","STMTL"],["E4","E"],["E4","FCALL"]],
+
+    # While loop
+    "WHL1": [["WHL","WHL2"]],
+    "WHL2": [["E2","WHL3"]],
+    "WHL3": [["E","WHL4"]],
+    "WHL4": [["E4","STMTL"],["E4","E"],["E4","FCALL"]],
+
 }
